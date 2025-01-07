0f23eb646199d379c43f925bc5eeafe8df69097a
Magnus Farstad <magnus.r.farstad@outlook.com>
2025-01-07 17:01:49 +0100

    add format time to no func

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,12 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function formatDateToNo(dateString: string): string {
+  const date = new Date(dateString);
+  return date.toLocaleDateString("no", {
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
+  });
+}
