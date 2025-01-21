4008db6e1ce7c5689699bc3ce2ff6582517956e7
Magnus Farstad <magnus.r.farstad@outlook.com>
2025-01-21 13:39:42 +0100

    add supabase db to allowed image paths

--- next.config.js
+++ next.config.js
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lwybipvgkqqcuevmabdl.supabase.co",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
   experimental: {
     serverActions: {
       bodySizeLimit: "6mb",
